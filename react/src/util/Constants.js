9076940b76e73d0f30bc3a49d8f8b257f679829e
Leonid <Malkiev96@gmail.com>
2021-01-23 16:57:32 +0500

    Add env

--- react/src/util/Constants.js
+++ react/src/util/Constants.js
@@ -1,12 +1,11 @@
-export const ADMIN_ID = 1;
-export const ADMIN_NAME = 'Leonid';
-export const ACCESS_TOKEN = 'accessToken';
-export const SORT_TYPE = 'sortType';
-export const SORT_DATE = 'createdDate,desc';
+export const ADMIN_ID = 1
+export const ADMIN_NAME = 'Leonid'
+export const ACCESS_TOKEN = 'accessToken'
+export const SORT_TYPE = 'sortType'
+export const SORT_DATE = 'createdDate,desc'
 
-export const BASE_API = 'http://localhost:8080';
-export const HOST = 'http://localhost:8080';
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
-export const GOOGLE_AUTH_URL = HOST + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const FACEBOOK_AUTH_URL = HOST + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const GITHUB_AUTH_URL = HOST + '/oauth2/authorize/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const BASE_API = process.env.REACT_APP_BACK_HOST
+export const OAUTH2_REDIRECT_URI = `${process.env.REACT_APP_FRONT_HOST}/oauth2/redirect`
+export const GOOGLE_AUTH_URL = `${BASE_API}/oauth2/authorize/google?redirect_uri=${OAUTH2_REDIRECT_URI}`
+export const FACEBOOK_AUTH_URL = `${BASE_API}/oauth2/authorize/facebook?redirect_uri=${OAUTH2_REDIRECT_URI}`
+export const GITHUB_AUTH_URL = `${BASE_API}/oauth2/authorize/github?redirect_uri=${OAUTH2_REDIRECT_URI}`
